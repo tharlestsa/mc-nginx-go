d27e85867e913d50f6fe2d159c75e8d9346c39d4
Tharles Andrade <irtharles@gmail.com>
2020-08-11 18:41:32 -0300

    change config

--- Dockerfile
+++ Dockerfile
@@ -10,7 +10,7 @@ COPY entrypoint.sh /entrypoint.sh
 RUN chmod +x /entrypoint.sh
 
 RUN chown -R www-data. /var/www/* && chmod -R ugo+w /var/www/*
-
+RUN sed "s/mapasculturais\:9000/$FASTCGI_HOSTNAME\:$FASTCGI_PORT/g" /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 ENTRYPOINT ["/entrypoint.sh"]
 
 #CMD ["nginx", "-g", "daemon off;"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,6 @@ set -e
 FASTCGI_HOSTNAME="${MAPAS_HOSTNAME:-mapasculturais}"
 FASTCGI_PORT="${MAPAS_PORT:-9000}"
 
-sed "s/mapasculturais\:9000/$FASTCGI_HOSTNAME\:$FASTCGI_PORT/g" /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+#sed "s/mapasculturais\:9000/$FASTCGI_HOSTNAME\:$FASTCGI_PORT/g" /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
 exec "$@"
