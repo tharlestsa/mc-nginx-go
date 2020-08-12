637709bf3eebf6f6476681481ab9a9eb15ae1d72
Tharles de Sousa Andrade <tharles.andrade@goias.gov.br>
2020-08-12 10:57:09 -0300

    change config

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,5 +12,7 @@
 set -e
 FASTCGI_HOSTNAME="${MAPAS_HOSTNAME:-mapasculturais}"
 FASTCGI_PORT="${MAPAS_PORT:-9000}"
-sed "s/mapasculturais\:9000/$FASTCGI_HOSTNAME\:$FASTCGI_PORT/g" /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+
+sed -i "s/mapasculturais\:9000/$FASTCGI_HOSTNAME\:$FASTCGI_PORT/g" /etc/nginx/conf.d/default.conf
+
 service nginx start
